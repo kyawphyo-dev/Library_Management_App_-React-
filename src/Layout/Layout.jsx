8ad49835b8f3d4fb3d1174431704919048179383
Kyaw Phyo Win <kyawphyowin242965@gmail.com>
2026-01-15 12:19:38 +0700

    create_project_and_setupRoute

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Outlet } from "react-router";
 
 export default function Layout() {
   return (
@@ -9,6 +10,7 @@ export default function Layout() {
           <li>Create</li>
         </ul>
       </nav>
+      <Outlet />
     </div>
   );
 }
