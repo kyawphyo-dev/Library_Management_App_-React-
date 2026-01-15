197b43ba38157a987d7680d5047b16dae3bc04cf
Kyaw Phyo Win <kyawphyowin242965@gmail.com>
2026-01-15 11:20:45 +0700

    install_and_setupRouter

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Create</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      <h1 className="text-3xl italic underline text-blue-950">Hello LMS</h1>
+    </>
+  );
+}
+
+export default Home;
